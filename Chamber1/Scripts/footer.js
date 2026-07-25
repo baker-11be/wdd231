50d258abb08cd7c525ad19ac0d96e75862ea5df2
baker-11b <bakermfitumukiza-email5@example.com>
2026-07-25 10:26:10 +0300

    commit

--- Chamber1/Scripts/footer.js
+++
@@ -1,7 +0,0 @@
-// ===== FOOTER DATE FUNCTIONS =====
-
-// Set current year
-document.getElementById('year').textContent = new Date().getFullYear();
-
-// Set last modified date
-document.getElementById('last-modified').textContent = document.lastModified;
